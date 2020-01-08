db515745f0cc9f00e029185d7714b79318e492cb
tlghk <tlghk@sihwancome>
2020-01-08 17:53:02 +0900

    0108

---
+++ java 0108/src/method/Student.java
@@ -0,0 +1,33 @@
+package method;
+
+public class Student {
+	// 인스턴스 변수 선언
+	private int num;
+	private String name;
+	private String major;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMajor() {
+		return major;
+	}
+
+	public void setMajor(String major) {
+		this.major = major;
+	}
+
+}
---
+++ java 0108/src/study/BoardTest.java
@@ -0,0 +1,17 @@
+package study;
+
+import org.junit.Test;
+
+public class BoardTest {
+
+	// 테스트 메소드 만들기
+	@Test
+	public void testMethod() {
+		board board = new board();
+		System.out.println(board);
+		
+		board board1 = new board();
+		System.out.println(board1);
+
+	}
+}
